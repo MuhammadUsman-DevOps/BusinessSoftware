1fd2c865d146f81ea96fb223f081a1ea2a5da9f9
Muhammad Usman <osmanamin689@gmail.com>
2022-06-29 05:13:32 -0700

    static data

--- Company/urls.py
+++ Company/urls.py
@@ -4,5 +4,16 @@ from Company import views
 
 urlpatterns = [
     path('base', views.company_base),
-    path('dashboard', views.dashboard, name="company_dashboard")
+    path('dashboard', views.dashboard, name="company_dashboard"),
+    path('settings/', views.company_settings, name="company_settings"),
+
+    path('investors/add', views.add_investor, name="add_investor"),
+    path('investors/', views.all_investors, name="all_investors"),
+
+    path('counter-parties/add', views.add_counter_party, name="add_counter_party"),
+    path('counter-parties/', views.all_counter_parties, name="all_counter_parties"),
+
+
+    path('bank-accounts/add', views.add_bank_account, name="add_bank_account"),
+    path('bank-accounts/', views.all_bank_accounts, name="all_bank_accounts"),
 ]
--- Company/views.py
+++ Company/views.py
@@ -7,3 +7,31 @@ def company_base(request):
 
 def dashboard(request):
     return render(request, template_name="Company/dashboard.html")
+
+
+def company_settings(request):
+    return render(request, template_name="Company/settings.html")
+
+
+def add_counter_party(request):
+    return render(request, template_name="Company/counter_party/add_counter_party.html")
+
+
+def add_investor(request):
+    return render(request, template_name="Company/investors/add_investors.html")
+
+
+def all_investors(request):
+    return render(request, template_name="Company/investors/all_investors.html")
+
+
+def all_counter_parties(request):
+    return render(request, template_name="Company/counter_party/counter_parties.html")
+
+
+def add_bank_account(request):
+    return render(request, template_name="Company/bank_accounts/add_bank_account.html")
+
+
+def all_bank_accounts(request):
+    return render(request, template_name="Company/bank_accounts/bank_accounts.html")
